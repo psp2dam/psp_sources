9beac2c50dc3fe6e7d9f07f667ba28c6b478571f
Vicente Martínez <vicente@iesdoctorbalmis.com>
2021-03-25 00:44:08 +0100

    Configuration changes

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -4,11 +4,11 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: 'Vuepress Docs Boilerplate',
+  title: 'IES Doctor Balmis - PSP (2ºDAM)',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
-  description: description,
+  description: 'Apuntes del módulo PSP de 2º curso del CFGS DAM',
 
   /**
    * Directory to be deployed to http://mydeploymenthost/base/
@@ -19,11 +19,13 @@ module.exports = {
    * Extra tags to be injected to the page HTML `<head>`
    *
    * ref：https://v1.vuepress.vuejs.org/config/#head
+   * Defaul theme-color: 3eaf7c
    */
   head: [
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'theme-color', content: '#3e7caf' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+    ['link', { rel: 'icon', href: '/psp-32x32.png' }]
   ],
 
   /**
@@ -32,11 +34,18 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
    */
   themeConfig: {
-    repo: '',
-    editLinks: false,
-    docsDir: '',
-    editLinkText: '',
-    lastUpdated: false,
+    // Smooth scrolling
+    smoothScroll: true,
+    // Github configuration
+    repo: 'psp2dam/psp_sources',
+    repoLabel: 'PSP Notes',
+    docsDir: 'src',
+    docsBranch: 'master',
+    editLinks: true,
+    editLinkText: 'Help us improve this page!',
+    lastUpdated: 'Last updated',
+    // Navigation bar configuration
+    logo: '/Logo_IES_Transparente_Blanco_v1-3',
     nav: [
       {
         text: 'Guide',
@@ -47,10 +56,11 @@ module.exports = {
         link: '/config/'
       },
       {
-        text: 'VuePress',
-        link: 'https://v1.vuepress.vuejs.org'
+        text: 'AULES',
+        link: 'https://aules.edu.gva.es/fp'
       }
     ],
+    // Sidebar configuration
     sidebar: {
       '/guide/': [
         {
@@ -70,6 +80,15 @@ module.exports = {
    */
   plugins: [
     '@vuepress/plugin-back-to-top',
-    '@vuepress/plugin-medium-zoom',
+    '@vuepress/plugin-medium-zoom': {
+      selector: 'img',
+      // medium-zoom options here
+      // See: https://github.com/francoischalifour/medium-zoom#options
+      options: {
+        margin: 48,
+        background: '#21253080',
+        scrollOffset: 40,
+      }
+    },
   ]
 }
